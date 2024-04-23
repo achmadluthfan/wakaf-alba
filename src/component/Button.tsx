754120a33c68b28bacf9a576abd75fa9929e9727
ahmadnurhidayat <nur26691@gmail.com>
2024-04-23 22:00:58 +0700

    edit button component

--- src/component/Button.tsx
+++ src/component/Button.tsx
@@ -8,7 +8,7 @@ interface ButtonProps {
 const Button: React.FC<ButtonProps> = ({ content, className }) => {
   return (
     <button
-      className={`px-[7vw] py-[2vw] border-2 text-[4vw] rounded-md border-[#016A70] ${className}`}
+      className={`px-[7vw] py-[2vw] border-2 text-[4vw] rounded-xl border-[#016A70] ${className}`}
     >
       {content}
     </button>
